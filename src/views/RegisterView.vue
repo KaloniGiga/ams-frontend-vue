e60e7a1eb7014d997d0530f8eb35ce80417eb56f
kalaunidipak <kalaunidipak5@gmail.com>
2024-09-02 04:03:18 +0000

    feat: router, login, register, and dasboard view setup

---
+++ src/views/RegisterView.vue
@@ -0,0 +1,29 @@
+
+<script setup>
+const handleFormSubmit = async (event) => {
+    const form = event.target
+    const formData = new FormData(form)
+    console.log(formData);
+}
+
+</script>
+<template>
+    <div :class="container">
+       <form @submit.prevent="handleFormSubmit">
+          <label for="email">Email</label>
+          <input type="text" name="email">
+           
+          <label>Password</label>
+          <input type="password" name="password">
+       </form>        
+    </div>
+</template>
+<style scoped>
+ .container {
+    width: "100%";
+    height: "100%";
+    display: "flex";
+    justify-content: "center";
+    align-items: "center";
+ }
+</style>
